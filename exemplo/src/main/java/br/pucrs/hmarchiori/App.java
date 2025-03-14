9609a592f8b7fd5716447a83b3f05d6505b46d2b
Henrique Corrales Marchiori <85183717+HMarchiori@users.noreply.github.com>
2025-03-14 00:56:45 +0000

    final changes

---
+++ exemplo/src/main/java/br/pucrs/hmarchiori/App.java
@@ -0,0 +1,13 @@
+package br.pucrs.hmarchiori;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
